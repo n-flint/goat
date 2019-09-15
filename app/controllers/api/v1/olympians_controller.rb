0c885bb920d58e73daff3b3842cf0deb0f7db006
Noah Flint <noah.flint24@gmail.com>
2019-09-15 17:23:06 -0600

    Added conditional to index method for age params

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::OlympiansController < ApplicationController
 
   def index
+    if params['age']
+      render json: Olympian.age_sort(params['age'])
+    else
     render json: Olympian.all
+    end
   end
 
 end
