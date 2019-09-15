557fd2753c16f75eeb7f6377a0e5845485d62834
Noah Flint <noah.flint24@gmail.com>
2019-09-15 15:25:11 -0600

    Changed validations to be capitalized

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,12 +1,12 @@
 class Olympian < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :sex
-  validates_presence_of :age
-  validates_presence_of :height
-  validates_presence_of :weight
-  validates_presence_of :team
-  validates_presence_of :games
-  validates_presence_of :sport
-  validates_presence_of :event
-  validates_presence_of :medal
+  validates_presence_of :Name
+  validates_presence_of :Sex
+  validates_presence_of :Age
+  validates_presence_of :Height
+  validates_presence_of :Weight
+  validates_presence_of :Team
+  validates_presence_of :Games
+  validates_presence_of :Sport
+  validates_presence_of :Event
+  validates_presence_of :Medal
 end
