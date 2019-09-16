1bceacfa4f3073e5e8e08d3ad3860df691ae53dd
Noah Flint <noah.flint24@gmail.com>
2019-09-16 15:09:25 -0600

    Added tests for avg weight male/female, avg age

--- spec/api/v1/models/olympian_spec.rb
+++ spec/api/v1/models/olympian_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Olympian, type: :model do
       Sex: 'M',
       Age: 100,
       Height: 150,
-      Weight: 50,
+      Weight: 150,
       Team: 'Team 1',
       Games: 'Game 1',
       Sport: 'Sport 1',
@@ -31,7 +31,7 @@ RSpec.describe Olympian, type: :model do
     @olympian_2 = Olympian.create(
       Name: 'olympian_2',
       Sex: 'M',
-      Age: 10,
+      Age: 200,
       Height: 150,
       Weight: 50,
       Team: 'Team 2',
@@ -39,17 +39,53 @@ RSpec.describe Olympian, type: :model do
       Sport: 'Sport 2',
       Event: 'Event 2',
       Medal: 'Medal 2'
+    ),
+    @olympian_3 = Olympian.create(
+      Name: 'olympian_3',
+      Sex: 'F',
+      Age: 100,
+      Height: 150,
+      Weight: 300,
+      Team: 'Team 3',
+      Games: 'Game 3',
+      Sport: 'Sport 3',
+      Event: 'Event 3',
+      Medal: 'Medal 3'
+    ),
+    @olympian_4 = Olympian.create(
+      Name: 'olympian_4',
+      Sex: 'F',
+      Age: 200,
+      Height: 150,
+      Weight: 100,
+      Team: 'Team 4',
+      Games: 'Game 4',
+      Sport: 'Sport 4',
+      Event: 'Event 4',
+      Medal: 'Medal 4'
     )
     end
 
     it '.age_sort' do
-    expect(Olympian.age_sort('youngest')).to eq(@olympian_2)
-    # will come back to this
-    # expect(Olympian.age_sort('oldest')).to eq(@olympian_1)
+      # will come back to this
+      # expect(Olympian.age_sort('youngest')).to eq(@olympian_1)
+      # expect(Olympian.age_sort('oldest')).to eq(@olympian_1)
     end
 
     it '.total_olympians' do
-    expect(Olympian.total_olympians).to eq(2)
+      expect(Olympian.total_olympians).to eq(4)
+    end
+
+    it '.avg_male_weight' do
+      expect(Olympian.avg_male_weight).to eq(100)
+    end
+
+    it '.avg_female_weight' do
+      expect(Olympian.avg_female_weight).to eq(200)
+    end
+
+    it '.avg_age' do
+      expect(Olympian.avg_age).to eq(150)
     end
   end
 end
