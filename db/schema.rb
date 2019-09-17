834bfbeabecb527f2bfd0c23531b3f35a37accbe
Noah Flint <noah.flint24@gmail.com>
2019-09-17 11:21:28 -0600

    Migrated events table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_165652) do
+ActiveRecord::Schema.define(version: 2019_09_17_171751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "Name"
+    t.string "Games"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "olympians", force: :cascade do |t|
     t.string "Name"
