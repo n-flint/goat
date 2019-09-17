0c99464f5951ad2c1d898ba047db0e77551ea3fc
Noah Flint <noah.flint24@gmail.com>
2019-09-17 11:06:48 -0600

    Migrated to add sports to olympians table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_164558) do
+ActiveRecord::Schema.define(version: 2019_09_17_165652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_164558) do
     t.string "Team"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "Sport"
   end
 
 end
