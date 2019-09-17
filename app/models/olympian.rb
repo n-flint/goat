0b9da0acc2ebc98f320170824159931dc0597bae
Noah Flint <noah.flint24@gmail.com>
2019-09-17 10:51:19 -0600

    Removed validations for unused columns

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -5,10 +5,6 @@ class Olympian < ApplicationRecord
   validates_presence_of :Height
   validates_presence_of :Weight
   validates_presence_of :Team
-  validates_presence_of :Games
-  validates_presence_of :Sport
-  validates_presence_of :Event
-  validates_presence_of :Medal
 
   def self.age_sort(params)
     if params == 'youngest'
