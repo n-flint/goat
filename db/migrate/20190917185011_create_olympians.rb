84b0b3b5451ca6ac1da3db67c01689c419bc0a5b
Noah Flint <noah.flint24@gmail.com>
2019-09-17 20:45:07 -0600

    Created migration for olympians table

--- db/migrate/20190917185011_create_olympians.rb
+++ db/migrate/20190917185011_create_olympians.rb
@@ -1,13 +1,13 @@
 class CreateOlympians < ActiveRecord::Migration[5.2]
   def change
     create_table :olympians do |t|
-      t.string :Name
-      t.string :Age
-      t.string :Sex
-      t.string :Height
-      t.string :Weight
-      t.string :Team
-      t.string :Sport
+      t.string :name
+      t.string :age
+      t.string :sex
+      t.string :height
+      t.string :weight
+      t.string :team
+      t.string :sport
 
       t.timestamps
       
