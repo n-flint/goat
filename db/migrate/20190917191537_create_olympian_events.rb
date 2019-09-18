8ffd935233b38b4a921b75c0d7076880ccc4746d
Noah Flint <noah.flint24@gmail.com>
2019-09-17 20:46:55 -0600

    Created migration for olympian events table

--- db/migrate/20190917191537_create_olympian_events.rb
+++ db/migrate/20190917191537_create_olympian_events.rb
@@ -1,7 +1,7 @@
 class CreateOlympianEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :olympian_events do |t|
-      t.string :Medals
+      t.string :medals
       t.references :olympian, foreign_key: true
       t.references :event, foreign_key: true
     end
