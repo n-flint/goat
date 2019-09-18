c27c9b0b1eeeede944c54a5f96990e7ede33239f
Noah Flint <noah.flint24@gmail.com>
2019-09-17 20:50:55 -0600

    Changed capitalization

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
-  validates_presence_of :Name
-  validates_presence_of :Games
+  validates_presence_of :name
+  validates_presence_of :games
+
+  has_many :olympian_events
+  has_many :olympians, through: :olympian_events
 end
